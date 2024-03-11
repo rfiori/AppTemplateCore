5d2227ca9ba580d7ee9dbc0bf032a466059d29fe
Ricardo <test@test.com>
2024-03-10 21:03:12 -0300

    Update

---
+++ src/AppTemplateCore.Domain/Interfaces/Repository/IPagingResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTemplateCore.Domain.Interfaces.Repository
+{
+    public interface IPagingResult<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity>? Items { get; }
+
+        int PageNumber { get; }
+
+        int PageSize { get; }
+
+        int TotalItems { get; }
+
+        int TotalPages { get; }
+    }
+}
---
+++ src/AppTemplateCore.Domain/Interfaces/Repository/_IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTemplateCore.Domain.Interfaces.Repository
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        Task<TEntity?> GetByIdAsync(Guid id);
+
+        Task<TEntity?> GetByIdAsync(int id);
+
+        int Add(TEntity obj);
+
+        Task<IPagingResult<TEntity>?> GetAllPagingAsync(int? pageSize, int? page);
+
+        [Obsolete("Use new method GetAllPagingAsync()")]
+        Task<IEnumerable<TEntity>?> GetAllAsync();
+
+        IPagingResult<TEntity>? FindPaging(Expression<Func<TEntity, bool>> predicate, int pageSize = 50, int page = 1);
+
+        Task<IEnumerable<TEntity>?> Find(Expression<Func<TEntity, bool>> predicate);
+
+        Task<TEntity?> Update(TEntity obj);
+
+        Task<int> Remove(Guid id);
+
+        Task<int> Remove(int id);
+
+        int SaveChanges();
+    }
+}
